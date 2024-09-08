627c5681273a6e674ed9eda15bd9ebd355c3b1bc
Valdera <f.valdera@yahoo.co.id>
2024-09-08 23:39:57 +0700

    ci: fix ci

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -175,26 +175,26 @@ function(setup_target_for_coverage_lcov)
     add_custom_target(
             ${Coverage_NAME}
             # Cleanup lcov
-            COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --ignore-errors mismatch
+            COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --ignore-errors unused,mismatch --gcov-tool ${GCOV_PATH}
             -directory . -b ${BASEDIR} --zerocounters
             # Create baseline to make sure untouched files show up in the report
-            COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --ignore-errors mismatch -c
+            COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --ignore-errors unused,mismatch --gcov-tool ${GCOV_PATH} -c
             -i -d . -b ${BASEDIR} -o ${Coverage_NAME}.base
             # Run tests
             COMMAND ${Coverage_EXECUTABLE} ${Coverage_EXECUTABLE_ARGS}
             # Capturing lcov counters and generating report
             COMMAND
-            ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --ignore-errors mismatch
+            ${LCOV_PATH} ${Coverage_LCOV_ARGS} --ignore-errors unused,mismatch --gcov-tool ${GCOV_PATH}
             --directory . -b ${BASEDIR} --capture --output-file
             ${Coverage_NAME}.capture
             # add baseline counters
             COMMAND
-            ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --ignore-errors mismatch -a
+            ${LCOV_PATH} ${Coverage_LCOV_ARGS} --ignore-errors unused,mismatch --gcov-tool ${GCOV_PATH} -a
             ${Coverage_NAME}.base -a ${Coverage_NAME}.capture --output-file
             ${Coverage_NAME}.total
             # filter collected data to final coverage report
             COMMAND
-            ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --ignore-errors mismatch --remove
+            ${LCOV_PATH} ${Coverage_LCOV_ARGS} --ignore-errors unused,mismatch --gcov-tool ${GCOV_PATH} --remove
             ${Coverage_NAME}.total ${LCOV_EXCLUDES} --output-file
             ${Coverage_NAME}.info
             # Generate HTML output
